61be74128c0d8a423df222587578e08de05274b0
03-junghee <03_junghee@naver.com>
2026-04-13 01:52:16 +0900

    edit 2

--- grab_survey/settings.py
+++ grab_survey/settings.py
@@ -108,8 +108,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# settings.py
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # 실제 static 폴더 위치
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- grab_survey/wsgi.py
+++ grab_survey/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for grab_survey project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
+# grab_survey/wsgi.py (또는 프로젝트명/wsgi.py)
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grab_survey.settings')
 
 application = get_wsgi_application()
+
+# Vercel이 인식할 수 있도록 application을 app으로 연결
+app = application
