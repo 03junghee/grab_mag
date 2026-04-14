49b2a98dbe71326d49a951780178c188c71d1015
03-junghee <03_junghee@naver.com>
2026-04-14 22:43:18 +0900

    fixfix

--- survey/utils.py
+++ survey/utils.py
@@ -1,19 +1,16 @@
 import requests
 
-def send_telegram_message(survey_data):
-    # 본인의 실제 정보로 교체하세요!
+# 함수 정의 부분 (보통 utils.py 또는 views.py 상단)
+def send_telegram_message(survey_data): # 인자를 1개만 받도록 수정
     bot_token = '8591374168:AAFEtiCnK4Nw8wRfMGKUShzqf4JoSbbR0wQ'
-    chat_id = '8412088648'  # 403 에러 방지를 위해 @userinfobot에서 받은 숫자 ID 입력
+    chat_id = '8412088648'
     
     message = "🔔 **[Grab Magazine] 새로운 인터뷰 응답 도착!**\n"
     message += "━━━━━━━━━━━━━━━━━━━━\n"
-    
+
     for key, value in survey_data.items():
-        # 질문 번호만 추출하거나 간략하게 표현
-        q_label = key.split('_')[0] 
-        q_title = key.split('_')[1] if '_' in key else ""
-        
-        message += f"**[{q_label}]** {q_title}\n"
+        # 이미 views.py에서 key에 질문 제목을 넣어줬으므로 그대로 출력합니다.
+        message += f"**{key}**\n"
         message += f"└ 답변: {value}\n\n"
     
     message += "━━━━━━━━━━━━━━━━━━━━"
@@ -21,8 +18,6 @@ def send_telegram_message(survey_data):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     
     try:
-        response = requests.post(url, data={'chat_id': chat_id, 'text': message, 'parse_mode': 'Markdown'})
-        if response.status_code != 200:
-            print(f"텔레그램 전송 실패: {response.text}")
+        requests.post(url, data={'chat_id': chat_id, 'text': message, 'parse_mode': 'Markdown'})
     except Exception as e:
         print(f"연결 오류: {e}")
--- survey/views.py
+++ survey/views.py
@@ -9,7 +9,7 @@ QUESTIONS = {
         'title': '',
         'sub_title': '20대 트렌드 매거진 그랩이라고 합니다!\n트렌드 리서치를 위한 그랩 길거리 인터뷰에\n응해주셔서 감사합니다 :)',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/95HntuS.jpeg',
         'placeholder': '몇 년 생이신가요?',
         'button_text': '> 인터뷰 시작!',
     },
@@ -18,13 +18,14 @@ QUESTIONS = {
         'sub_title': '길거리 인터뷰 콘텐츠 트렌드!\n알고 계시나요?',
         'type': 'radio',
         'options': ['나에게 온다면 어떻게 대답할지 상상한다.', '본 적도 들은 적도 없다.', '기타'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/95HntuS.jpeg',
+        'button_text': '> 다음 문제',
     },
     2: {  
         'title': 'Q2',
         'sub_title': '응답자의 MBTI를 알려주세요!',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/enLyB7z.jpeg',
         'placeholder': 'MBTI',
         'button_text': '> 다음 문제',
     },
@@ -32,7 +33,7 @@ QUESTIONS = {
         'title': 'Q3',
         'sub_title': '그랩은 혼족과 같은 메가트렌드를 예측하고자 합니다 :D\n요즘 나는 어떤 하루를 보내고,\n어떤 생각을 자주 하나요?',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/x2frzKD.jpeg',
         'placeholder': '텍스트를 입력해 주세요.',
         'button_text': '> 다음 문제',
     },
@@ -41,13 +42,14 @@ QUESTIONS = {
         'sub_title': '[취미 트렌드 조사]\n취미는 나에게?',
         'type': 'radio',
         'options': ['없으면 안돼! 스트레스를 푸는 수단~', '있으면 좋지~ 행복을 더해주는 수단!'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/jCcLrxE.jpeg',
+        'button_text': '> 다음 문제',
     },
     5: { 
         'title': 'Q5',
         'sub_title': '[취미 트렌드 조사]\n어떤 취미가 있으신가요?',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/JViBj6Y.jpeg',
         'placeholder': '텍스트를 입력해 주세요.',
         'button_text': '> 다음 문제',
     },
@@ -56,13 +58,14 @@ QUESTIONS = {
         'sub_title': '[취미 트렌드 조사]\n나는 트렌드를 즐길 때?',
         'type': 'radio',
         'options': ['얕고 넓게 다양한 것을 즐기는 편!', '깊고 좁게, 진심으로 덕질하는 편!'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/B8rqei5.jpeg',
+        'button_text': '> 다음 문제',
     },
     7: {  
         'title': 'Q7',
         'sub_title': '[취미 트렌드 조사]\n최근 즐겼던 트렌드의 흐름이 있다면요?',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/B8rqei5.jpeg',
         'placeholder': 'like 두쫀쿠,,,버터떡,,,',
         'button_text': '> 다음 문제',
     },
@@ -70,7 +73,7 @@ QUESTIONS = {
         'title': 'Q7',
         'sub_title': '[취미 트렌드 조사]\n하루의 언제 주로 취미를 즐기시나요?',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/dHICZCX.jpeg',
         'placeholder': '텍스트를 입력해 주세요.',
         'button_text': '> 다음 문제',
     },
@@ -79,27 +82,30 @@ QUESTIONS = {
         'sub_title': '[인테리어 트렌드 조사]\n내가 자취하면 화이트톤 vs 우드톤 vs 블랙실버모던',
         'type': 'radio',
         'options': ['올 화이트톤 !', '푸릇 싱싱 우드톤!', '블랙 실버 모던톤!', '나만의 독보적인 취향✨'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/3UkcpDh.jpeg',
+        'button_text': '> 다음 문제',
     },
     10: {
         'title': 'Q10',
         'sub_title': '[인테리어 트렌드 조사]\n잠깐! 무지성 밸겜 TIME!',
         'type': 'radio',
         'options': ['내가 애인 집 가기', '애인이 내 집 오기'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/GHFHJ03.jpeg',
+        'button_text': '> 다음 문제',
     },
     11: {
         'title': 'Q11',
         'sub_title': '[여행 트렌드 조사]\n생각만 해도 행복한 여름 방학/휴가!\n무엇을 하며 보내실 예정인가요?',
         'type': 'checkbox',
         'options': ['여행/휴식 :)', '비자발적 일정 :(', '미래를 위한 준비 !', '기타'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/PHJxzec.jpeg',
+        'button_text': '> 다음 문제',
     },
     12: {  
         'title': 'Q12',
         'sub_title': '[여행 트렌드 조사]\n구체적인 계획이 있다면 알려주세요!',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/Je7XRHy.jpeg',
         'placeholder': '텍스트를 입력해 주세요.',
         'button_text': '> 다음 문제',
     },
@@ -108,27 +114,30 @@ QUESTIONS = {
         'sub_title': '[20대 생각 트렌드 조사]\n잠깐! QUIZE TIME!\n1년 후의 나는 어떤 모습일 것 같나요?',
         'type': 'radio',
         'options': ['나만의 추구미 실현하기', '롤모델 모습에 한발짝 다가가기', '버킷릿스트 n개 이루기'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/0fYSUmd.jpeg',
+        'button_text': '> 다음 문제',
     },
     14: {
         'title': 'Q14',
         'sub_title': '[20대 생각 트렌드 조사]\n최근 나만의 고민 트렌드가 있다면?',
         'type': 'radio',
         'options': ["'나 이거 잘 맞나'진로 고민!", "'저 사람은 뭐가 문젤까'인간관계 고민!", "'뭐 먹고 살지'인생 고민!"],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/JWuloxb.jpeg',
+        'button_text': '> 다음 문제',
     },
     15: {
         'title': 'Q15',
         'sub_title': '[20대 생각 트렌드 조사]\n요즘 관심 가는 트렌드를 골라주세요!',
         'type': 'checkbox',
         'options': ['피젯토이(키캡, 스트레스볼,,,)', '디저트(두쫀쿠, 버터떡,,,)', '거지모먼트(도시락, 절약 릴스,,,)', '주식(국장, 미장, 전쟁주,,,)', '타투(미니, 레터링, 올드스쿨,,,)', '기타'],
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/JWuloxb.jpeg',
+        'button_text': '> 다음 문제',
     },
     16: {  
         'title': 'Q16',
         'sub_title': '[20대 생각 트렌드 조사]\n나를 움직이게 하는 원동력은 무엇인가요?',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/b1iyYam.png',
         'placeholder': '텍스트를 입력해 주세요.',
         'button_text': '> 다음 문제',
     },
@@ -136,9 +145,34 @@ QUESTIONS = {
         'title': 'Q17',
         'sub_title': '[20대 생각 트렌드 조사]\n요즘 가장 오래 붙잡고 있는 고민은?\n그 고민이 시작된 계기는 무엇인가요?',
         'type': 'text', 
-        'img': 'thank_you_icon.png',
+        'img': 'https://i.imgur.com/JzFEjRr.png',
         'placeholder': '텍스트를 입력해 주세요.',
         'button_text': '> 다음 문제',
+    },
+    18: {
+        'title': '인터뷰 만족도',
+        'sub_title': '참여해주셔서 감사합니다 :)\n인터뷰 만족도 조사 한번 부탁드려요!',
+        'type': 'score', 
+        'img': 'https://i.imgur.com/oFKU5Ef.jpeg',
+        'button_text': '다음 문제',
+    },
+    19: {
+        'title': '인터뷰어',
+        'type': 'text',
+        'placeholder': '홍길동',
+        'button_text': '다음 문제',
+    },
+    20: {
+        'title': '인터뷰 응답자',
+        'type': 'text',
+        'placeholder': '감사합니다♡',
+        'button_text': '다음 문제',
+    },
+    21: {
+        'title': '인터뷰 응답자 연락처',
+        'type': 'text',
+        'placeholder': '010-0000-0000(선택)',
+        'button_text': '완료하기',
     },
 }
 
@@ -157,22 +191,32 @@ def home(request):
 def survey_step(request, step):
     if request.method == 'POST':
         data = request.session.get('survey_data', {})
-        # 질문 제목과 함께 저장해서 텔레그램에서 보기 편하게 만듭니다.
-        q_title = QUESTIONS[step]["title"]
-        data[f"{step}. {q_title}"] = request.POST.get('answer')
+        
+        question_info = QUESTIONS.get(step)
+        q_title = question_info.get("title", f"Q{step}")
+        q_sub_title = question_info.get("sub_title", "").replace('\n', ' ')
+        
+        # 데이터 저장
+        data[f"[{step}. {q_title}] {q_sub_title}"] = request.POST.get('answer')
         request.session['survey_data'] = data
         
-        if step < 17:  # 이제 17단계까지 갑니다!
+        if step < 21:  
             return redirect('survey_step', step=step + 1)
         else:
-            # 17번 답변까지 완료 후 텔레그램 발송
+            # 1. 텔레그램 발송
             send_telegram_message(data)
+            
+            # 2. [핵심] 발송 성공 후 세션 데이터 삭제
+            # 이렇게 해야 다음 사람이 참여할 때 이전 데이터와 섞이지 않습니다.
+            if 'survey_data' in request.session:
+                del request.session['survey_data']
+            
             return redirect('result')
-    
-    # 해당 스텝의 질문 데이터 전달
+        
     context = {
         'step': step,
         'question': QUESTIONS.get(step),
+        'score_range': range(11),
     }
     return render(request, 'survey/survey_step.html', context)
 
